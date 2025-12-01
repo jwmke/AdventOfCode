389905e5a30f0afb36dd811900efb65fe04f86d1
Joe <joseph5062a@gmail.com>
2025-11-30 22:31:16 -0800

    day1 p2

--- 2025/day1.py
+++ 2025/day1.py
@@ -9,11 +9,27 @@ def solve() -> int:
     file = open('input.txt', 'r')
     read = file.readlines()
     stripped = list(map(lambda s : s.replace("\n", ""), read))
-    t = 0
+    current_pos = 50
+    inst = []
+    zero_count = 0
     for i, l in enumerate(stripped):
-        print(l)
+        inst.append((l[0], int(l[1:])))
 
-    return t
+    for lr, amount in inst:
+        if lr == "L":
+            for _ in range(amount):
+                current_pos -= 1
+                current_pos = current_pos % 100
+                if current_pos == 0:
+                    zero_count += 1
+        elif lr == "R":
+            for _ in range(amount):
+                current_pos += 1
+                current_pos = current_pos % 100
+                if current_pos == 0:
+                    zero_count += 1
+
+    return zero_count
 
 if __name__ == '__main__':
     print(solve())
--- template.py
+++ template.py
@@ -9,25 +9,11 @@ def solve() -> int:
     file = open('input.txt', 'r')
     read = file.readlines()
     stripped = list(map(lambda s : s.replace("\n", ""), read))
-    current_pos = 50
-    inst = []
-    zero_count = 0
+    t = 0
     for i, l in enumerate(stripped):
-        inst.append((l[0], int(l[1:])))
+        print(l)
 
-    for lr, amount in inst:
-        if lr == "L":
-            current_pos -= amount
-            current_pos = current_pos % 100
-            if current_pos < 0:
-                current_pos = 100 - current_pos
-        elif lr == "R":
-            current_pos += amount
-            current_pos = current_pos % 100
-        if current_pos == 0:
-            zero_count+=1
-
-    return zero_count
+    return t
 
 if __name__ == '__main__':
     print(solve())
