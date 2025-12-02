53e92a3395b81b0e2f0de3b3cf6808a934fcf14a
Joe <joseph5062a@gmail.com>
2025-12-01 21:37:05 -0800

    day2 p2

--- 2025/day2.py
+++ 2025/day2.py
@@ -9,7 +9,7 @@ def solve() -> int:
     file = open('input.txt', 'r')
     read = file.readlines()
     stripped = list(map(lambda s : s.replace("\n", ""), read))
-    t = 0
+    tf = set()
     id_ranges = []
     for r in stripped[0].split(","):
         x, y = r.split("-")
@@ -18,13 +18,23 @@ def solve() -> int:
     for r1, r2 in id_ranges:
         for i in range(r1, r2+1):
             si = str(i)
-            if len(si) % 2 == 0:
-                if si[:len(si)//2] == si[len(si)//2:]:
-                    t+=i
-    
-    
+            css = []
+            cur_sub_strings = []
+            for ix in range(len(si)//2):
+                css.append(si[ix])
+                cur_sub_strings.append(''.join(css))
+            for ss in cur_sub_strings:
+                if len(si)%len(ss)==0:
+                    all_same = True
+                    for ssi in range(0, len(si), len(ss)):
+                        sss = si[ssi:ssi+len(ss)]
+                        if sss != ss:
+                            all_same = False
+                            break
+                    if all_same:
+                        tf.add(i)
 
-    return t
+    return sum(tf)
 
 if __name__ == '__main__':
     print(solve())
