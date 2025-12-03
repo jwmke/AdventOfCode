e3b2546b110a73670c28a0dd1f5ed128f4438518
Joseph Weller <joseph5062a@gmail.com>
2025-12-03 00:51:02 -0800

    day3 p2

--- 2025/day3.py
+++ 2025/day3.py
@@ -10,23 +10,17 @@ def solve() -> int:
     read = file.readlines()
     stripped = list(map(lambda s : s.replace("\n", ""), read))
     t = 0
-    for i, l in enumerate(stripped):
-        m1, m2 = 0,0
-        mi = 0,
-        for ic, c in enumerate(l):
-            if int(c) > m1:
-                mi = ic
-                m1 = int(c)
-        if mi == len(l)-1:
-            for ic, c in enumerate(l[:-1]):
-                if int(c) > m2:
-                    m2 = int(c)
-            t += int(''.join([str(m2), str(m1)]))
-        else:
-            for ic, c in enumerate(l[mi+1:]):
-                if int(c) > m2:
-                    m2 = int(c)
-            t += int(''.join([str(m1), str(m2)]))
+    for _, l in enumerate(stripped):
+        fil = []
+        ll = list(l)
+        for i in range(12, 0, -1):
+            cl = ll[:((-i)+1)]
+            if i == 1:
+                cl = ll
+            mv = max(cl)
+            ll = ll[cl.index(mv)+1:]
+            fil.append(mv)
+        t+= int(''.join(fil))
 
     return t
 
