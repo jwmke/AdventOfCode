782034146df58bd2e34a65fdc859dac2199e3341
Joe <joseph5062a@gmail.com>
2025-12-06 22:17:08 -0800

    day7 p2

--- 2025/day7.py
+++ 2025/day7.py
@@ -9,26 +9,28 @@ def solve() -> int:
     file = open('input.txt', 'r')
     read = file.readlines()
     stripped = list(map(lambda s : s.replace("\n", ""), read))
-    t = 0
     g = []
-    dl = set()
+    ix = 0
     for i, l in enumerate(stripped):
         g.append(l)
         if i == 0:
             for x in range(len(l)):
                 if l[x] == "S":
-                    dl.add(x)
-    for y in range(len(g)):
-        if y == 0:
-            continue
-        for x, c in enumerate(g[y]):
-            if x in dl and c == "^":
-                t+=1
-                dl.add(x-1)
-                dl.add(x+1)
-                dl.remove(x)
+                    ix = x
 
-    return t
+    @functools.cache
+    def timeline(y, x, t):
+        if y == len(g):
+            return 1
+        if g[y][x] == "^":
+            l = timeline(y, x-1, t+1)
+            r = timeline(y, x+1, t+1)
+            return l + r
+        else:
+            return timeline(y+1, x, t)
+        
+
+    return timeline(1, ix, 0)
 
 if __name__ == '__main__':
     print(solve())
