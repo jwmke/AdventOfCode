c23dd721cebc6649ca4d32a770302cadb25e1133
Joe <joseph5062a@gmail.com>
2025-12-09 21:32:26 -0800

    day10 p1

--- 2025/day10.py
+++ 2025/day10.py
@@ -3,6 +3,7 @@ import sys
 import copy
 from collections import defaultdict
 import functools
+from itertools import combinations
 
 def solve() -> int:
     sys.setrecursionlimit(200000)
@@ -10,8 +11,38 @@ def solve() -> int:
     read = file.readlines()
     stripped = list(map(lambda s : s.replace("\n", ""), read))
     t = 0
+    jolts = []
+    end_states = []
+    buttons = []
     for i, l in enumerate(stripped):
-        print(l)
+        ll = l.split()
+        cb = []
+        for b in ll[1:-1]:
+            cb.append(b[1:-1].split(","))
+        buttons.append(cb)
+        end_states.append(ll[0][1:-1])
+        jolts.append(ll[-1][1:-1].split(","))
+
+    for si in range(len(end_states)):
+        es = list(end_states[si])
+        bs = buttons[si]
+        f = False
+        for i in range(1, len(bs)+1):
+            if f == True:
+                break
+            cc = combinations(bs, i)
+            for c in cc:
+                cs = ['.']*len(es)
+                for b in c:
+                    for bp in b:
+                        if cs[int(bp)] == '.':
+                            cs[int(bp)] = '#'
+                        elif cs[int(bp)] == '#':
+                            cs[int(bp)] = '.'
+                if es == cs:
+                    t+=len(c)
+                    f = True
+                    break
 
     return t
 
